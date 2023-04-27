8082032e30559fa34508bd03aa9999913dfc591e
LucyMiMi <lucymur.dev@gmail.com>
2023-04-27 13:10:33 +0300

    college-test-2

--- index.js
+++ index.js
@@ -16,35 +16,44 @@ const content = fs.readFileSync(path.join(
 
 // BEGIN
 const solution = (content) => {
+
   const data = content.split('\r\n').slice(1, content.split('\r\n').length);
   const detailsData = data.map((el) => el.split('|').filter((el) => el).map((el) => el.trim()));
   // 1
   const count = data.length;
   console.log(`Сколько всего видов существ: ${count}`);
   // 2
+  const priceForOne = (list) => Number(list[list.length-1]) / Number(list[3]);
   const countOfSU = 10;
   const countOfSSU = 20;
   const maxStrength = Math.max(...detailsData.map((el) => el[1]));
   const sUnit = detailsData.filter((el) => el.includes(String(maxStrength))).flat();
   const sSUnit = detailsData.filter((el) => !el.includes(String(maxStrength))).flat();
-  const price = Number(sUnit[sUnit.length-1]) * countOfSU + Number(sSUnit[sSUnit.length-1]) * countOfSSU;
-  console.log(`Цена найма 10 самых сильных и 20 вторых по силе: ${price}`);
-  // 3
-  const currency = 10000;
-  const squads = detailsData.reduce((acc, el) => {
-    const count = (currency / el[el.length-1])
-    return [...acc, [count * el[1], el[0], count]];
-  }, []);
-    const strongestSquad = Math.max(...squads.map((el) => el[0]));
-    const strongestType = squads.filter((el) => el.includes(strongestSquad)).flat();
-  console.log(`Отряд за 10000: ${strongestType[2]} ${strongestType[1]}`);
+  const price = priceForOne(sUnit) * countOfSU + priceForOne(sSUnit) * countOfSSU;
+  console.log(`Цена найма 10 самых сильных существ и 20 существ вторых по силе: ${price}`);
+  // 3 
+  const fattest = detailsData.filter((el) => el.includes(String(Math.max(...detailsData.map((el) => el[3]))))).flat();
+  const thinnest = detailsData.filter((el) => el.includes(String(Math.min(...detailsData.map((el) => el[3]))))).flat();
+  const priceFattestAndThinnest = Number(fattest[fattest.length-1]) + Number(thinnest[thinnest.length-1]);
+  console.log(`Самый толстый юнит: ${fattest[0]}, Самый худой юнит: ${thinnest[0]}, Стоимость найма этих отрядов: ${priceFattestAndThinnest}`)
   // 4
   const coefUnits = detailsData.reduce((acc, el) => [...acc, [el[1] / el[el.length-1], el[0]]], []);
   const profitableCoef = Math.max(...coefUnits.map((el) => el[0]));
   const unprofitableCoef = Math.min(...coefUnits.map((el) => el[0]));
   const profitableUnit = coefUnits.filter((el)=>el.includes(profitableCoef)).flat();
   const unprofitableUnit = coefUnits.filter((el)=>el.includes(unprofitableCoef)).flat();
-  console.log(`Самые выгодные: ${profitableUnit[1]}, Самые невыгодные: ${unprofitableUnit[1]}`);
+  console.log(`Самые невыгодные: ${unprofitableUnit[1]}, Самые выгодные: ${profitableUnit[1]}`);
+  //5
+  const currency = 10000;
+  const squads = detailsData.reduce((acc, el) => {
+    const countSquads = (currency / el[el.length-1]);
+    const strOfSquad = countSquads * Number(el[3]) * Number(el[1]);
+    return [...acc, [strOfSquad, el[0]]];
+  }, []);
+    const strongestSquad = Math.max(...squads.map((el) => el[0]));
+    const strongestType = squads.filter((el) => el.includes(strongestSquad)).flat();
+  console.log(`Отряд за 10000: ${strongestType[0]} ${strongestType[1]}`);
+  
 };
 
 solution(content);
