83238664015da965641e8fcbc00d6ef855eec57e
LucyMiMi <lucymur.dev@gmail.com>
2023-05-02 09:54:28 +0300

    fix

--- index.js
+++ index.js
@@ -23,27 +23,22 @@ const solution = (content) => {
   console.log(`Сколько всего видов существ: ${data.length}`);
   // 2
   const priceForOne = (list) => Number(list[list.length-1]);
-  const countOfSU = 10;
-  const countOfSSU = 20;
   const maxStrength = Math.max(...detailsData.map((el) => el[1]));
   const sUnit = detailsData.filter((el) => el[1] === String(maxStrength)).flat();
   const sSUnits = detailsData.filter((el) => el[1] !== String(maxStrength));
-  const maxStrengthSSU = Math.max(...sSUnits.map((el) => el[1]));
-  const sSUnit = sSUnits.filter((el) => el[1] === String(maxStrengthSSU)).flat();
-  const price = priceForOne(sUnit) * countOfSU + priceForOne(sSUnit) * countOfSSU;
-  console.log(`Цена найма 10 самых сильных существ и 20 существ вторых по силе: ${Math.round(price)}`);
+  const sSUnit = sSUnits.filter((el) => el[1] === String(Math.max(...sSUnits.map((el) => el[1])))).flat();
+  console.log(`Цена найма 10 самых сильных: ${priceForOne(sUnit) * 10}. Самый сильный: ${sUnit[0]}. Цена найма 20 вторых по силе: ${priceForOne(sSUnit)*20}, второй по силе: ${sSUnit[0]}`);
+ 
   // 3 
   const fattest = detailsData.filter((el) => el[5] === String(Math.max(...detailsData.map((el) => el[5])))).flat();
-  const fattestSquadPrice = Number(fattest[3]) * priceForOne(fattest);
   const thinnest = detailsData.filter((el) => el[5] === String(Math.min(...detailsData.map((el) => el[5])))).flat();
-  const thinnestSquadPrice = Number(thinnest[3]) * priceForOne(thinnest);
-  console.log(`Самый толстый юнит: ${fattest[0]}. Cтоимость найма отряда ${fattest[0]}ов: ${fattestSquadPrice}, Самый худой юнит: ${thinnest[0]}. Стоимость найма отряда ${thinnest[0]}ов: ${thinnestSquadPrice}`)
+  console.log(`Самый толстый юнит: ${fattest[0]}. Cтоимость найма отряда ${fattest[0]}ов: ${Number(fattest[3]) * priceForOne(fattest)}, Самый худой юнит: ${thinnest[0]}. Стоимость найма отряда ${thinnest[0]}ов: ${Number(thinnest[3]) * priceForOne(thinnest)}`)
   // 4
   const coefUnits = detailsData.reduce((acc, el) => [...acc, [el[1] / el[el.length-1], el[0]]], []);
   const profitableCoef = Math.max(...coefUnits.map((el) => el[0]));
   const unprofitableCoef = Math.min(...coefUnits.map((el) => el[0]));
-  const profitableUnit = coefUnits.filter((el)=>el.includes(profitableCoef)).flat();
-  const unprofitableUnit = coefUnits.filter((el)=>el.includes(unprofitableCoef)).flat();
+  const profitableUnit = coefUnits.filter((el) => el.includes(profitableCoef)).flat();
+  const unprofitableUnit = coefUnits.filter((el) => el.includes(unprofitableCoef)).flat();
   console.log(`Самые невыгодные: ${unprofitableUnit[1]}, Самые выгодные: ${profitableUnit[1]}`);
   //5
   const currency = 10000;
